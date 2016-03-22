c1a6fa9df5315c2fa30ac3b1737390afbbb5d9ba
ravuthlong <ravuthlong@yahoo.com>
2016-03-22 14:38:39 -0700

    Input size

--- Controller2.java
+++ Controller2.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class Controller2 
 {
 	public static void main(String[] args)
 	{
+		System.out.println("Welcome to connect 4");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the board size: ");
+		int boardSize = input.nextInt();
+		System.out.println("Enter the connection size: ");
+		int winningSize = input.nextInt();
 		ViewSwing tmp = new ViewSwing(10, 5);
 	}
 }
